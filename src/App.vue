6bf1ce00221cccb272dc823f5bf3ab666d69e820
Roberto Rugama <roberto.rugama01@gmail.com>
2018-08-01 20:26:01 -0600

    events
    uso de eventos

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,18 @@
       <p v-else-if="false">{{ 'algo mas' }}</p>
       <p v-else> {{ 'Lo ultimo' }}</p>
 
+      <input v-model="name">
+      <input v-model="lasName">
+      <input type="date" v-model="date">
+      <input type="date" v-model="date2">
+      <p>{{cumpleanos}}</p>
+      <p>{{ fullName }}</p>
+      <button v-on:click="format">Formatear</button>
+
+      <p> {{ formattedName }}</p>
+
+      <a v-bind:href="url">Mi Correo</a>
+
       <ul >
         <li v-for="i in items">{{ i }}</li>
       </ul>
@@ -22,9 +34,32 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       showValue :false,
       value:  'algo',
-      items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+      items: [1,2,3,4,5],
+      name: '',
+      lasName: '',
+      date:'',
+      date2:'',
+      formattedName: '',
+      
+      url: 'https://mail.google.com/mail/u/0/#inbox'
+    } 
+  },
+    computed: {
+      fullName (){
+        return this.name +' '+ this.lasName
+      },
+
+      cumpleanos (){
+        
+      return this.date - this.date2;
+      }
     }
-  }
+    ,
+    methods: {
+      format(){
+        this.formattedName = this.name.split(' ').join('/').toUpperCase()
+      }
+    }
 }
 </script>
 
