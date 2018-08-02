928217f65410ee177a029b2d9e831a2b04f7ab30
Roberto Rugama <roberto.rugama01@gmail.com>
2018-08-01 18:08:51 -0600

    basico
    v-show and v-if and v-else-if

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template lang="pug">
  
-    #app
-      img(src="./assets/logo.png")
-      h1 {{ msg}}
-      h2 Essential Links
+    <div id="app">
+      <img src="./assets/logo.png" alt="">
+      <h1>{{msg}}</h1>
+      <h2>Esentianl Links</h2>
+      <p v-show="showValue">{{value}}</p>
+      <p v-if="showValue">{{value}}</p>
+      <p v-else-if="false">{{ 'algo mas' }}</p>
+      <p v-else> {{ 'Lo ultimo' }}</p>
 
-      p {{1+1}}
-      p {{'Hola'+'Mundo'}}
-
-      p {{ person.name }}
-
-      p {{ person.name.toUpperCase() }}
-
-      p {{ JSON.stringify(person) }}
-      p {{true? 'true':'false' }}
-  
+      <ul >
+        <li v-for="i in items">{{ i }}</li>
+      </ul>
+    </div>
 </template>
 <script>
 export default {
@@ -22,10 +20,9 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-
-      person: {
-        name:'Roberto'
-      }
+      showValue :false,
+      value:  'algo',
+      items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     }
   }
 }
