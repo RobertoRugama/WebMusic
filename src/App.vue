61c7e564a02516ffcfc356c4c8da4642437aba68
Roberto Rugama <roberto.rugama01@gmail.com>
2018-08-02 00:22:52 -0600

    dgfd
    dgf

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,33 @@
-<template lang="pug">
- 
+<template>
     <div id="app">
-      <img src="./assets/logo.png" alt="">
-      <h1>{{msg}}</h1>
-      <h2>Esentianl Links</h2>
-      <p v-show="showValue">{{value}}</p>
-      <p v-if="showValue">{{value}}</p>
-      <p v-else-if="false">{{ 'algo mas' }}</p>
-      <p v-else> {{ 'Lo ultimo' }}</p>
-
-      <input v-model="name">
-      <input v-model="lasName">
-      <input type="date" v-model="date">
-      <input type="date" v-model="date2">
-      <p>{{cumpleanos}}</p>
-      <p>{{ fullName }}</p>
-      <button v-on:click="format">Formatear</button>
-
-      <p> {{ formattedName }}</p>
-
-      <a v-bind:href="url">Mi Correo</a>
-
-      <ul >
-        <li v-for="i in items">{{ i }}</li>
-      </ul>
-    </div>
+      <section class="section">
+       <nav class="nav has-shadow">
+        <div class="container">
+          <input class="input is-large" type="text", placeholder="Buscar Musicas", v-model="searchQuery">
+          <a class="button is-info is-large" @click="search">Buscar</a>
+          <a class="button is-danger is-large">&times;</a>
+        </div>
+        <div class="container columns colum">
+          
+        </div>
+      </nav>
+      
+    </section>
+  </div>
 </template>
 <script>
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      showValue :false,
-      value:  'algo',
-      items: [1,2,3,4,5],
-      name: '',
-      lasName: '',
-      date:'',
-      date2:'',
-      formattedName: '',
-      
-      url: 'https://mail.google.com/mail/u/0/#inbox'
-    } 
+      searchQuery: ''
+    }
   },
-    computed: {
-      fullName (){
-        return this.name +' '+ this.lasName
-      },
-
-      cumpleanos (){
-        
-      return this.date - this.date2;
-      }
+  methods: {
+    search(){
+      console.log(this.searchQuery)
     }
-    ,
-    methods: {
-      format(){
-        this.formattedName = this.name.split(' ').join('/').toUpperCase()
-      }
-    }
+  }
 }
 </script>
 
